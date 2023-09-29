922c5c00f361b53650c3421c78db43f7270a23d7
RemiFELIN <remi.felin@gmail.com>
2023-09-29 18:53:37 +0200

    fix env

--- rdfmining-front/src/tools/env.js
+++ rdfmining-front/src/tools/env.js
@@ -1,17 +1,8 @@
 import io from "socket.io-client";
 
-console.log(process.env)
-var nodeEnv = process.env.NODE_ENV || "development";
 console.log("~~~")
-console.log(nodeEnv)
+console.log(process.env.NODE_ENV)
 console.log("~~~")
 
-let endpoint = "";
-if (nodeEnv === "production") {
-    endpoint =  "http://134.59.130.136";
-} else if (nodeEnv === "development") {
-    endpoint =  "http://localhost:9200";
-}
-
-export const base = endpoint;
-export const socket = nodeEnv === "production" ? io(base + "/io") : io(base);
+export const base = process.env.NODE_ENV === "production" ? "http://134.59.130.136" : "http://localhost:8890";
+export const socket = process.env.NODE_ENV === "production" ? io("http://134.59.130.136/ws") : io("http://localhost:8891");
