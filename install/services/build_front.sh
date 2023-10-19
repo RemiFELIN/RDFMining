3d314225b93a24d4541083a92d79fefbb12cefb9
RemiFELIN <remi.felin@gmail.com>
2023-10-19 11:07:52 +0200

    update repo

--- install/services/build_front.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo $(date +"%Y-%m-%d %H:%M:%S,%3N")" [install.sh] INFO - Build RDFMiner front ..."
-docker-compose build front
-echo $(date +"%Y-%m-%d %H:%M:%S,%3N")" [install.sh] INFO - Done !"
