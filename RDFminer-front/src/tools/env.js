81545906546b6462fd2ac7849b4975f07aeeb7b4
RemiFELIN <remi.felin@gmail.com>
2024-02-07 19:17:18 +0100

    fix env issue

--- RDFminer-front/src/tools/env.js
+++ RDFminer-front/src/tools/env.js
@@ -1,8 +1,9 @@
 import io from "socket.io-client";
+require('dotenv').config();
 
 console.log("~~~")
-console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV)
 console.log("~~~")
 
-export const base = process.env.RDFMINER_FRONT_ENDPOINT;
-export const socket = io(process.env.RDFMINER_FRONT_SOCKET_ENDPOINT);
+export const base = process.env.RDFMINER_FRONT_ENDPOINT
+export const socket = io(process.env.RDFMINER_FRONT_SOCKET_ENDPOINT)
